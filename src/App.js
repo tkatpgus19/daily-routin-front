76a85effe3dbcfb7ffc0a73a97d35e4c3c6dfce0
sehyun <germs1020@gmail.com>
2023-04-14 02:55:32 +0900

    style: 앱, 리스트 서랍 스타일 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header>
+        <nav>navbar</nav>
+      </header> */}
+      <div className='content'>
+        <div className='application-area'>
+          <div className='application-container'>
+            <div className='application-icon'>
+              <h1>운동목록</h1>
+              <p>하이</p>
+            </div>
+            <div className='application-icon'>
+              <h1>운동목록</h1>
+              <p>하이</p>
+            </div>
+            <div className='application-icon'>
+              <h1>운동목록</h1>
+              <p>하이</p>
+            </div>
+            <div className='application-icon'>
+              <h1>운동목록</h1>
+              <p>하이</p>
+            </div>
+          </div>
+        </div>
+        <div className='specification-area'>
+          <div className='specification-container'>
+            <div className='specification-icon'>
+              <h3>운동목록</h3>
+              <p>하이1</p>
+            </div>
+            <div className='specification-icon'>
+              <h3>운동목록</h3>
+              <p>하이1</p>
+            </div>
+            <div className='specification-icon'>
+              <h3>운동목록</h3>
+              <p>하이1</p>
+            </div>
+            <div className='specification-icon'>
+              <h3>운동목록</h3>
+              <p>하이1</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <div className='project-info'>
+          <p>이 프로젝트는 오직 개인적인 목적을 위해 진행되었습니다. 자유롭게 수정, 배포해도 상관이 없으며 깃허브 주소만 남겨주세요.</p>
+          <p>This project was carried out for personal purposes only. It doesn't matter if you modify or distribute it freely, just leave the address of GitHub.</p>
+        </div>
+      </footer>
     </div>
   );
 }
